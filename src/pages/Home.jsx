81c70d2d5b776041478837da3e7af134f5412c1e
William Larsson <williaamlarsson@gmail.com>
2020-04-19 11:57:12 +0200

    Moved to pages folder.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CasePreview from '../components/CasePreview'
+import Collaborators from '../components/Collaborators'
+
+function Home() {
+    return (
+        <div>
+            <CasePreview />
+            <Collaborators />
+        </div>
+    )
+}
+
+export default Home
